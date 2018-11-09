fff679c27cb9c5671783b9c6656a1c6403a69ed7
bDango <zhengdk@qq.com>
2018-11-09 12:07:25 +0800

    Finish login and logout

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use Eloquent;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * @mixin Eloquent
+ */
 class User extends Authenticatable
 {
     use Notifiable;
@@ -32,4 +36,5 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
 }
